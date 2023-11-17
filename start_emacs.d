909d73f072da7165d0a4fe3f58ffae2d15dcba76
kenandrews <46874890+kenandrews@users.noreply.github.com>
2023-11-16 20:22:21 -0600

    Create start_emacs.d

---
+++ start_emacs.d
@@ -0,0 +1,7 @@
+;; Place this file in C:\Users\eaand\AppData\Roaming and point to the appropriate files
+
+(setq user-init-file "C:\Users\eaand\.emacs.d\init.el")
+(setq user-emacs-directory "C:\Users\eaand\.emacs.d")
+(setq default-directory "C:\Users\eaand\")
+(setenv "HOME" "C:\Users\eaand\")
+(load user-init-file)
