d679af94cf180cb2aaf9133833bdf29424a8a56a
Andrews <ANDREWKE@ad.reyrey.com>
2023-11-21 11:06:23 -0600

    added to init.el

---
+++ elpa/nerd-icons-20231115.1110/nerd-icons-pkg.el
@@ -0,0 +1,14 @@
+(define-package "nerd-icons" "20231115.1110" "Emacs Nerd Font Icons Library"
+  '((emacs "24.3"))
+  :commit "cfda588dc1e865a8a121b9071f3720fd4937492b" :authors
+  '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("lisp")
+  :url "https://github.com/rainstormstudio/nerd-icons.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
